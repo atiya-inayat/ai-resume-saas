257c839499819be57b5c06dd7ff40e528ce49487
atiya-inayat <atiyainayat869@gmail.com>
2025-12-04 15:27:05 +0500

    initial setup of project

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <span>Home</span>
+      <span>About</span>
+      <span>Contact us</span>
+    </div>
+  );
+};
+
+export default Navbar;
